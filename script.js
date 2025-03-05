ea4fbb1215bfae5bc929b8e1be19e63c6d22330c
MaartenSlebos <maarten.slebos@hotmail.com>
2025-03-05 15:59:54 +0100

    add hover effect

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 
-
-
-
-
 for (let i = 0; i < 256; i++) {
     const container = document.querySelector('.container')
     const square = document.createElement('div')
     square.style.backgroundColor = 'black'
     square.style.height = '25px'
     square.style.width = '25px'
+
+    square.addEventListener('mouseenter', function() {
+        square.style.backgroundColor = 'red';
+    });
+
+    square.addEventListener('mouseleave' , function() {
+        square.style.backgroundColor = 'black';
+    })
+
     container.append(square)
 }
 
