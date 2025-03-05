d6ad85e4e34ccf5514d9c093ee2139f61858dc79
MaartenSlebos <maarten.slebos@hotmail.com>
2025-03-05 17:22:13 +0100

    choose grid size button

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
+const container = document.querySelector('.container');
 
-for (let i = 0; i < 256; i++) {
-    const container = document.querySelector('.container')
-    const square = document.createElement('div')
-    square.style.backgroundColor = 'black'
-    square.style.height = '25px'
-    square.style.width = '25px'
+function makeGrid(size) {
+    container.innerHTML = '';
+    const squareSize = 400 / size;
+    for (let i = 0; i < size * size; i++ ) {
+        const square = document.createElement('div');
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+        square.style.backgroundColor = 'black';
 
-    square.addEventListener('mouseenter', function() {
-        square.style.backgroundColor = 'red';
-    });
-
-    container.append(square)
+        square.addEventListener('mouseenter', function() {
+            square.style.backgroundColor = 'red';
+        });
+        container.append(square);
+    }
 }
 
+function createNewGrid() {
+    const input = prompt('Enter number of squares per side (max 100):');
+    const size = parseInt(input);
 
+    if (isNaN(size) || size < 1 || size > 100) {
+        alert('Please enter a valid number between 1 and 100.');
+        return; 
+    }
+    makeGrid(size);
+}
+
+makeGrid(16);
 
