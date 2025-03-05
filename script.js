dba9aa07d1960bcc7f720a88334ed1dd3f1990bc
MaartenSlebos <maarten.slebos@hotmail.com>
2025-03-05 15:45:17 +0100

    append 256 squares to container

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+for (let i = 0; i < 256; i++) {
+    const container = document.querySelector('.container')
+    const square = document.createElement('div')
+    square.style.backgroundColor = 'black'
+    square.style.height = '25px'
+    square.style.width = '25px'
+    container.append(square)
+}
+
+
+
